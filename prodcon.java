bc7ab827d18b831be8e7d3ea5a57b84540783436
abubakr1934 <115393179+abubakr1934@users.noreply.github.com>
2023-08-19 07:52:59 +0530

    Add files via upload

--- prodcon.java
+++ prodcon.java
@@ -1,71 +1,71 @@
-importjava.util.*;
-public class producerconsumer1 {
-public static void main(String[] args) throws InterruptedException {
-ArrayList<Integer> queue=new ArrayList<>();
-int size=5;
-consumer c=new consumer(queue, size);
-Thread pthread=new Thread(new producer(queue, size), "producer");
-Thread cthread=new Thread(c, "consumer");
-pthread.start();
-Thread.currentThread().sleep(10);
-cthread.start();
-}
-}
-class producer implements Runnable{
-ArrayList<Integer> queue=new ArrayList<>();
-final int size;
-public producer(ArrayList<Integer> queue, int size) {
-this.queue=queue;
-this.size=size;
-}
-@Override
-public void run() {
-for(int i=0;i<7;i++) {
-if(queue.size()==size) // checks for queue full
-{
-synchronized(queue)
-{
-System.out.println("queue is full producer is waiting" );
-try {
-queue.wait();
-} catch (InterruptedException ex) {
-System.out.println("interrupted"); }
-}
-}
-synchronized(queue)
-{
-System.out.println("produced " + i + queue.add(i));
-queue.notifyAll();
-}
-}
-}
-}
-class consumer implements Runnable{
-ArrayList<Integer> queue;
-final int size;
-boolean flag=true;
-public consumer(ArrayList<Integer> queue, int size) {
-this.queue=queue;
-this.size=size;
+import java.util.ArrayList;
 
+public class ProducerConsumerWithThreads {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> queue = new ArrayList<>();
+        int size = 5;
+        Consumer c = new Consumer(queue, size);
+        Thread producerThread = new Producer(queue, size);
+        Thread consumerThread = new Thread(c, "consumer");
+        producerThread.start();
+        Thread.sleep(10); // Sleep for a short time to allow producerThread to start
+        consumerThread.start();
+    }
 }
-@Override
-public void run() {
-while(true){
-while(queue.isEmpty())
-{
-synchronized(queue){
-try {
-queue.wait();
-} catch (InterruptedException ex) {
-System.out.println("interrupted");
+
+class Producer extends Thread {
+    ArrayList<Integer> queue;
+    final int size;
+
+    public Producer(ArrayList<Integer> queue, int size) {
+        this.queue = queue;
+        this.size = size;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 7; i++) {
+            synchronized (queue) {
+                while (queue.size() == size) {
+                    System.out.println("Queue is full, producer is waiting");
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException ex) {
+                        System.out.println("Interrupted");
+                    }
+                }
+                System.out.println("Produced " + i);
+                queue.add(i);
+                queue.notifyAll();
+            }
+        }
+    }
 }
+
+class Consumer implements Runnable {
+    ArrayList<Integer> queue;
+    final int size;
+
+    public Consumer(ArrayList<Integer> queue, int size) {
+        this.queue = queue;
+        this.size = size;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (queue) {
+                while (queue.isEmpty()) {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException ex) {
+                        System.out.println("Interrupted");
+                    }
+                }
+                int k = queue.remove(0);
+                System.out.println("Consumed " + k);
+                queue.notifyAll();
+            }
+        }
+    }
 }
-}
-synchronized(queue)
-{
-queue.notifyAll();
-int k=queue.remove(0);
-System.out.println("consumed " + k);
-}
-}} }
